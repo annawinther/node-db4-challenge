bbb5481138323d1189b65b344c917109a1974ae5
Anna W <anski.anna@gmail.com>
2019-07-18 17:43:40 +0200

    db queries made

--- data/seeds/04-quantity.js
+++ data/seeds/04-quantity.js
@@ -5,11 +5,14 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('ingredients_quantity').insert([
-        { recipe_id: 1, ingredient_id: 2, quantity: Math.random()*10},
-        { recipe_id: 1, ingredient_id: 1, quantity: Math.random()*10},
-        { recipe_id: 3, ingredient_id: 1, quantity: Math.random()*10},
-        { recipe_id: 2, ingredient_id: 3, quantity: Math.random()*10},
-        { recipe_id: 1, ingredient_id: 2, quantity: Math.random()*10},
+        { recipe_id: 1, ingredient_id: 2, quantity: 5},
+        { recipe_id: 1, ingredient_id: 1, quantity: 3},
+        { recipe_id: 3, ingredient_id: 1, quantity: 6},
+        { recipe_id: 2, ingredient_id: 3, quantity: 2},
+        { recipe_id: 1, ingredient_id: 4, quantity: 2},
+        { recipe_id: 1, ingredient_id: 5, quantity: 7},
+        { recipe_id: 1, ingredient_id: 6, quantity: 5},
+        { recipe_id: 1, ingredient_id: 7, quantity: 1},
       ]);
     });
 };
